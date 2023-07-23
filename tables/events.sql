118b65909cc3d4c68ac8a3eabe97ef17e8cc8fd6
Cai Parry-Jones <97813242+caitpj@users.noreply.github.com>
2023-07-23 18:34:55 +0100

    Change events.sql to work on Redshift

--- tables/events.sql
+++ tables/events.sql
@@ -1,6 +1,24 @@
-CREATE TABLE events(
-    user_id: Integer,
-    event_type: VARCHAR,
-    event_timestamp: TIMESTAMP
-    event_date: Date
+create table database_name.schema_name.events(
+    user_id Integer,
+    event_type VARCHAR,
+    event_date Date
 )
+;
+
+
+insert into database_name.schema_name.events values
+    (1, 'like', '2023-01-01'),
+    (1, 'like', '2023-01-02'),
+    (1, 'like', '2023-01-03'),
+    (1, 'share', '2023-01-04'),
+    (1, 'comment', '2023-01-05'),
+    (1, 'comment', '2023-01-30'),
+    (1, 'comment', '2023-01-31'),
+    (2, 'like', '2023-01-30'),
+    (2, 'share', '2023-01-31'),
+    (3, 'like', '2023-01-31')
+;
+
+
+select * from database_name.schema_name.events
+;
