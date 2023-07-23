a8a5a9d3af68d52e05db7f1fb9b20503a046d9a0
Cai Parry-Jones <97813242+caitpj@users.noreply.github.com>
2023-07-23 18:50:14 +0100

    Change active_users_daily.sql to work on Redshift

--- tables/active_users_daily.sql
+++ tables/active_users_daily.sql
@@ -1,8 +1,46 @@
-create table active_users_cumulated (
-    user_id: integer,
-    is_active_today: integer,
-    num_likes: integer,
-    num_comments: integer,
-    num_shares: integer,
-    snapshot_date: date
+create table scratchpad.data_eng.cai_test_active_users_cumulated (
+    user_id integer,
+    is_active_today integer,
+    num_likes integer,
+    num_comments integer,
+    num_shares integer,
+    snapshot_date date
 )
+;
+
+insert into scratchpad.data_eng.cai_test_active_users_cumulated values
+(1, 1, 1, 0, 0, '2023-01-01'),
+(1, 1, 1, 0, 0, '2023-01-02'),
+(1, 1, 1, 0, 0, '2023-01-03'),
+(1, 1, 0, 0, 1, '2023-01-04'),
+(1, 1, 0, 1, 0, '2023-01-05'),
+(1, 0, 0, 0, 0, '2023-01-06'),
+(1, 0, 0, 0, 0, '2023-01-07'),
+(1, 0, 0, 0, 0, '2023-01-08'),
+(1, 0, 0, 0, 0, '2023-01-09'),
+(1, 0, 0, 0, 0, '2023-01-10'),
+(1, 0, 0, 0, 0, '2023-01-11'),
+(1, 0, 0, 0, 0, '2023-01-12'),
+(1, 0, 0, 0, 0, '2023-01-13'),
+(1, 0, 0, 0, 0, '2023-01-14'),
+(1, 0, 0, 0, 0, '2023-01-15'),
+(1, 0, 0, 0, 0, '2023-01-16'),
+(1, 0, 0, 0, 0, '2023-01-17'),
+(1, 0, 0, 0, 0, '2023-01-18'),
+(1, 0, 0, 0, 0, '2023-01-19'),
+(1, 0, 0, 0, 0, '2023-01-20'),
+(1, 0, 0, 0, 0, '2023-01-21'),
+(1, 0, 0, 0, 0, '2023-01-22'),
+(1, 0, 0, 0, 0, '2023-01-23'),
+(1, 0, 0, 0, 0, '2023-01-24'),
+(1, 0, 0, 0, 0, '2023-01-25'),
+(1, 0, 0, 0, 0, '2023-01-26'),
+(1, 0, 0, 0, 0, '2023-01-27'),
+(1, 0, 0, 0, 0, '2023-01-28'),
+(1, 0, 0, 0, 0, '2023-01-29'),
+(1, 1, 0, 1, 0, '2023-01-30'),
+(2, 1, 1, 0, 0, '2023-01-30')
+;
+
+select * from scratchpad.data_eng.cai_test_active_users_cumulated
+;
