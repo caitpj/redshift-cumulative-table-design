4606b453ecac1ea455e3dd502f990d4fd3767274
Cai Parry-Jones <97813242+caitpj@users.noreply.github.com>
2023-07-23 21:10:21 +0100

    Add sort key

--- tables/events.sql
+++ tables/events.sql
@@ -3,6 +3,9 @@ create table database_name.schema_name.events(
     event_type VARCHAR,
     event_date Date
 )
+-- Sort key is important since we only want to scan the most recent event_date for each run
+sortkey
+    (event_date);
 ;
 
 insert into database_name.schema_name.events values
