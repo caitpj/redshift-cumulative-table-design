b14b1371113ce7d205bcc2b395e5038daa18d454
Cai Parry-Jones <97813242+caitpj@users.noreply.github.com>
2023-07-30 13:19:01 +0100

    refactor for Redshift

--- queries/active_users_daily_populate.sql
+++ queries/active_users_daily_populate.sql
@@ -1,14 +1,17 @@
-TODO
-INSERT INTO active_users_daily
+insert into scratchpad.data_eng.cai_test_active_users_daily (
+    select
+        user_id,
+        -- If the user_id has at least 1 event, they are daily active
+        1 as is_active_today,
+        count(case when event_type = 'like' then 1 END) as num_likes, -- Use 'count' rather than 'sum' to get 0 instead of NULL
+        count(case when event_type = 'comment' then 1 END) as num_comments,
+        count(case when event_type = 'share' then 1 END) as num_shares,
+        '2023-01-31' as snapshot_date
+    from scratchpad.data_eng.cai_test_events
+    where event_date = '2023-01-31'
+    group by user_id
+)
+;
 
-SELECT
-    user_id,
-    -- If the user_id has at least 1 event, they are daily active
-    IF(COUNT(user_id) > 0, 1, 0) as is_active_today,
-    COUNT(CASE WHEN event_type = 'like' THEN 1 END) as num_likes,
-    COUNT(CASE WHEN event_type = 'comment' THEN 1 END) as num_comments,
-    COUNT(CASE WHEN event_type = 'share' THEN 1 END) as num_shares,
-    CAST('2022-01-01' AS DATE) as snapshot_date
-FROM events
-WHERE event_date = '2022-01-01'
-GROUP BY user_id
+select * from scratchpad.data_eng.cai_test_active_users_daily
+;
