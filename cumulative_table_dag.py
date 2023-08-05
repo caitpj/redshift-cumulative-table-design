6c8e47d37b1ba152e62648c8b0210c5c0dfe4474
Cai Parry-Jones <97813242+caitpj@users.noreply.github.com>
2023-08-05 15:31:21 +0100

    Refactor cumulative_table_dag.py for redshift

--- cumulative_table_dag.py
+++ cumulative_table_dag.py
@@ -1,30 +1,47 @@
 TODO
 from airflow import DAG
-from airflow.contrib.operators.presto_operator import PrestoOperator
+from airflow.providers.amazon.aws.operators.redshift_data import RedshiftDataOperator
+
+cumulated_sql_list = [
+    "active_users_cumulated_populate_1.sql",
+    "active_users_cumulated_populate_2.sql",
+    "active_users_cumulated_populate_3.sql",
+    "active_users_cumulated_populate_4.sql",
+]
 
 default_args = {
-    'owner': 'Data with Zach',
+    'owner': 'Cai Parry-Jones',
     # Very important to have your cumulative DAGs be set `depends_on_past = True`
     'depends_on_past': True,
-    'email': ['airflow@example.com'],
 }
 with DAG(
     'cumulative_table_example_dag',
     default_args=default_args,
-    description='A simple cumulative table example',
+    description='A cumulative table example',
     schedule_interval='@daily',
-    start_date='2022-01-01',
+    start_date='2023-01-31',
     catchup=False,
-    tags=['data with zach, cumulative table example'],
+    tags=['example'],
 ) as dag:
-    daily_task = PrestoOperator(
+    daily_task = RedshiftDataOperator(
+        aws_conn_id="connection id here",
+        region="region name here",
+        database="database name here",
         task_id='compute_active_user_daily',
         sql='queries/active_users_daily_populate.sql'
+        wait_for_completion=True,
     )
 
-    cumulative_task = PrestoOperator(
-        task_id='compute_active_users_cumulated',
-        sql='queries/active_users_cumulated_populate.sql'
+    cumulative_task = RedshiftDataOperator(
+        aws_conn_id="connection id here",
+        region="region name here",
+        database="database name here",
+        task_id="compute_active_users_cumulated",
+        sql=[
+            # Using jinja to run each of the SQL scripts one after anopther in the cumulated_sql_list
+            f"{{% include 'queries/{sql}' %}}" for sql in cumulated_sql_list
+        ],
+        wait_for_completion=True,
     )
 
     # the daily task has to run before the cumulative task
